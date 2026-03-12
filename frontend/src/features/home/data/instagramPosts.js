719c74e46ea5b864350384f384c995a1c39a963a
abraham <abrahamkifle1212@gmail.com>
2026-03-12 17:14:53 +0300

    feat(home): add mock data for products, collections, and social feed
    - Created collection.js with featured and categorized collection data.
    - Added product.js containing sample items with pricing and category metadata.
    - Added instagramPosts.js with image mappings for the social media section.
    - Added collectionBanner.js for banner-specific promotional data.

---
+++ frontend/src/features/home/data/instagramPosts.js
@@ -0,0 +1,43 @@
+import img4_bracket from "../../../assets/image/image (4).png";
+import img4 from "../../../assets/image/image 4.png";
+import img5 from "../../../assets/image/image 5.png";
+import img6 from "../../../assets/image/image 6.png";
+import img7 from "../../../assets/image/image 7.png";
+
+export const instagramPosts = [
+  {
+    _id: "inst1",
+    image: img4_bracket,
+    instagramUrl: "https://instagram.com/p/ABC123",
+    caption: "Summer outfit inspiration",
+    order: 1
+  },
+  {
+    _id: "inst2",
+    image: img4,
+    instagramUrl: "https://instagram.com/p/XYZ456",
+    caption: "Street fashion style",
+    order: 2
+  },
+  {
+    _id: "inst3",
+    image: img5,
+    instagramUrl: "https://instagram.com/p/QWE789",
+    caption: "Minimalist look",
+    order: 3
+  },
+  {
+    _id: "inst4",
+    image: img6,
+    instagramUrl: "https://instagram.com/p/RTY012",
+    caption: "Brunch essentials",
+    order: 4
+  },
+  {
+    _id: "inst5",
+    image: img7,
+    instagramUrl: "https://instagram.com/p/UIO345",
+    caption: "Weekend getaway vibes",
+    order: 5
+  }
+];
