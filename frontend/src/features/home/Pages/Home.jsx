6c183a5a6ff30ebed2a3ed0512c4a1bf85e4afd6
abraham <abrahamkifle1212@gmail.com>
2026-03-12 17:21:08 +0300

    feat: Create initial Home page component with Navbar integration.

--- frontend/src/features/home/Pages/Home.jsx
+++ frontend/src/features/home/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Banner from '../components/Banner/Banner'
+import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
-      <Banner />
+      <Navbar />
     </div>
   )
 }
